bfd21fca2174dc5eabc56f76cb0809fc2ccbc6db
Chris Nuernberger <chris@thinktopic.com>
2016-02-05 10:44:28 -0700

    Initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject caffe-protobuf "0.1.0"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.google.protobuf/protobuf-java "2.6.1"]]
+
+  :java-source-paths ["java"])
