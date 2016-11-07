bce5ff8fa029b5b5ccf50666c3a5b6c010c6323c
Chris Nuernberger <chris@thinktopic.com>
2016-11-07 10:57:08 -0700

    Correctly labelling project.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject caffe-protobuf "0.2.0"
+(defproject thinktopic/caffe-protobuf "0.2.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
